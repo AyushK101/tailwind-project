859aee627ea13001143c6f559100cfca34a5027c
ayush kumar <socialacc211@gmail.com>
2024-09-10 01:30:05 +0530

    tailwind practice

---
+++ note.md
@@ -0,0 +1,2 @@
+-tailwind is mobile first : css written without any breakpoints apply to mobile first ,
+    then breakpoints like sm , md etc
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./assignment.html'],
+  theme: {
+    
+    extend: {
+      colors:{
+        'ayush' : '#000'
+      },
+      fontFamily:{
+        'ayush-font' : ['Protest Guerrilla','Inter','Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
